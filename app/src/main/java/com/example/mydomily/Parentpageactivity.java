f211f2f5885e0dc7e8492fbab218887bf5b5a033
gracielamedina <alfredo.ramos@csedge.org>
2021-07-21 09:47:12 -0400

    combine our code

---
+++ app/src/main/java/com/example/mydomily/Parentpageactivity.java
@@ -0,0 +1,14 @@
+package com.example.mydomily;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Parentpageactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parentpageactivity);
+    }
+}
